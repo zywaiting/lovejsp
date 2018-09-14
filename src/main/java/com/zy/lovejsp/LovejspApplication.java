f7401ae16b653d4333f342eda59610957177ed02
wq_zy <1573240324@qq.com>
2018-09-14 16:12:18 +0800

    ...

---
+++ src/main/java/com/zy/lovejsp/LovejspApplication.java
@@ -0,0 +1,12 @@
+package com.zy.lovejsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovejspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovejspApplication.class, args);
+    }
+}
